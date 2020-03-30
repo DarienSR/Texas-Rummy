f6e5bd53f9c90b9902d0a43782d1eec0534681ec
DarienSR <dariensloan@gmail.com>
2020-03-30 19:11:58 -0400

    Shuffle Deck, Deal Starting Cards, Pull Cards

--- src/Board.js
+++ src/Board.js
@@ -38,16 +38,37 @@ export default class Board extends Component {
 
   async componentDidMount() {
     const res = await this.setDeck();
-    let deck = res.map((card) => {
-      return <Card suit={card.suit} value={card.value} image={card.image} />
-    })
+    let deck = res.map((card) => { return <Card suit={card.suit} value={card.value} image={card.image} /> })
+    let p1 = [];
+    let p2 = [];
 
-    // shuffle deck
-
+    // shuffle deck, our own shuffle algoritm. 
+    shuffle(deck)
     // deal 3 cards to each player
-
+    for(let i = 0; i < 3; i++) {
+      // pops card from top of the deck and puts it in players hand
+      p1.push(deck.shift());
+      p2.push(deck.shift());
+    }
     // set deck to remaining cards
-    this.setState({deck: deck})
+    this.setState({deck: deck, playerOneHand: p1, playerTwoHand: p2})
+    function shuffle(arr) {
+        for(let i = 0; i < 1000; i++) {
+          // generate two random numbers
+          let x = Math.floor(Math.random() * 52)
+          let y = Math.floor(Math.random() * 52)
+          // swap two random indexs in our dex.
+          let temp = arr[x];
+          arr[x] = arr[y];
+          arr[y] = temp;
+        }
+        return arr
+    }
+  }
+
+  PullCard() {
+    let card = this.state.deck.shift();
+    this.setState({playerOneHand: [...this.state.playerOneHand, card]})
   }
 
   render() {
@@ -58,12 +79,14 @@ export default class Board extends Component {
         
           <div className="Card-Pile">
             {this.state.deck[8]}
-            <button onClick={this.PullCard}>
+            <button onClick={() => this.PullCard()}>
               <img className="pull" src="https://i.pinimg.com/originals/11/45/96/11459640e599fd105943f5a379d25248.jpg" />
             </button>
+            {/* <h2>Score</h2> */}
           </div>
 
           <Player hand={this.state.playerOneHand} />
+          <button>Discard</button>
         </div>
       </div>
     )
--- src/Player.js
+++ src/Player.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import "./Player.css";
 
 export default class Player extends Component {
   render() {
     return (
-      <div>
-        
+      <div className="Player">
+        {this.props.hand.map((card) => {
+          return card;
+        })}
       </div>
     )
   }
